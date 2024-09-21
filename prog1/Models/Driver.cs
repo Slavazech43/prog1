2c8476adad24e853a9111d7c74285e48c4e9e66a
TemeronX <Slava230805@gmail.com>
2024-09-21 21:31:54 +0700

    Создал классы Driver и DriverAction

---
+++ prog1/Models/Driver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog1.Models
+{
+    internal class Driver
+    {
+        string name;
+    }
+}
---
+++ prog1/Models/DriverAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog1.Models
+{
+    internal class DriverAction
+    {
+    }
+}
